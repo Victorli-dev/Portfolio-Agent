a242bd76d77e3bd8b82d2dbde8d5a7edc489fc3f
Victor Li <vito@Victors-MacBook-Air.local>
2026-03-24 11:41:57 -0400

    Text changes and adding guardrails

--- App.py
+++ App.py
@@ -1,8 +1,8 @@
 import streamlit as st
 from Agent.Setup import initialize_kernel, create_client
 
-st.set_page_config(page_title="Document Index Agent", layout="centered")
-st.title("Document Index Agent")
+st.set_page_config(page_title="Ask anything about Victor>", layout="centered")
+st.title("Victor's Personal AI Agent")
 
 config = initialize_kernel()
 client = create_client()
@@ -24,6 +24,12 @@ if prompt := st.chat_input("Ask something..."):
             response = client.messages.create(
                 model=config["model"],
                 max_tokens=1024,
+                system = 
+                '''You are an assistant designed specifically to 
+                answer questions about victor li. Answer only using things in your knoweldge base.
+                If you do not have the knowledge in your knowledgebase please respond with. I don't have that information 
+                on Victor Li
+                ''',
                 messages=[
                     {"role": m["role"], "content": m["content"]}
                     for m in st.session_state.messages
